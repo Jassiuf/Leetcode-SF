037b5a26c804c3e2e564c9bc2c1dcc5fefb22fe5
Jassi <jaswinder777singh@gmail.com>
2018-08-28 16:13:18 -0400

    Add files via upload

---
+++ 3Sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i <= nums.length - 3; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])){
+                int target = -nums[i];
+                int lo = i + 1;
+                int hi = nums.length - 1;
+                while (lo < hi) {
+                    int sum = nums[lo] + nums[hi];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i] , nums[lo] , nums[hi] ));
+                        while (lo < hi && nums[lo] == nums[lo+1]) lo++;
+                        while (lo < hi && nums[hi] == nums[hi-1]) hi--;
+                        lo++;
+                        hi--;
+                    } 
+                    else if (sum > target) hi--;
+                    else lo++;
+           } 
+        }            
+     }
+        return res;
+    }
+}    
